df5d8f057fd0f5b12b624d9209f8e0488c67c52f
Michael Sweet <michael@3dmarcomms.com>
2017-12-04 15:32:55 +0000

    Day 1 Part 2

---
+++ day1_part2.py
@@ -0,0 +1,11 @@
+puzzle_input = str(input("Puzzle input: "))
+
+input_length = len(puzzle_input)
+input_length_half = input_length / 2
+total = 0
+for i in range(0, input_length):
+    next_i = (i + input_length_half) % input_length
+    if puzzle_input[i] == puzzle_input[next_i]:
+        total = total + int(puzzle_input[i])
+
+print total
